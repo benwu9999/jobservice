47673bdc85e46654141259c14fb0bc6d286310f5
youlepan <youlepan@gmail.com>
2017-04-13 14:22:38 +0000

    modify class name to camel case

--- JobPostService/mysite/settings.py
+++ JobPostService/mysite/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'jobpostapp.apps.jobpostappConfig',
+    'jobpostapp.apps.JobPostAppConfig',
 ]
 
 MIDDLEWARE = [
