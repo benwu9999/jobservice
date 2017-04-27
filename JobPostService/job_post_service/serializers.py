6156590a40cdfe41c4e11c66cd16812ccb512db3
benwu <benwu9999@gmail.com>
2017-04-26 21:53:06 -0400

    save compensation only if it doesn't exists, created gitignore file

--- JobPostService/job_post_service/serializers.py
+++ JobPostService/job_post_service/serializers.py
@@ -24,7 +24,10 @@ class JobPostSerializer(serializers.ModelSerializer):
 
     def create(self, validated_data):
         compensation_data = validated_data.pop('compensation')
+
+        # create and insert compensation into db ONLY if it doesn't already exists, using "get_or_create"
         compensation, created = Compensation.objects.get_or_create(**compensation_data)
+
         jobPost = JobPost.objects.create(compensation=compensation, **validated_data)
         self.is_valid(raise_exception=True)
         jobPost.save()
