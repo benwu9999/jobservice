db07f6d8e7c0f28ce662f053a157e8b0bc28954a
benwu <benwu9999@gmail.com>
2017-06-06 19:24:09 -0400

    fixed create endpoint

--- JobPostService/job_post_app/urls.py
+++ JobPostService/job_post_app/urls.py
@@ -1,18 +1,19 @@
-
 from  django.conf.urls import url
 from rest_framework.urlpatterns import format_suffix_patterns
 from . import views
 
-urlpatterns=[
-	url(r'^$', views.index, name='index'),
+urlpatterns = [
+    url(r'^$', views.index, name='index'),
 
-	# need the '/' before $ to properly route call to generics.ListCreateAPIView
-	url(r'^jobPost/$', views.JobPostList.as_view()),
+    url(r'^jobPost/compensations$', views.CompensationList.as_view()),
+    url(r'^jobPost/allIds$', views.AllIdsList.as_view()),
 
-	# supports /jobPost/{jobPostId}
-	url(r'^jobPost/(?P<jobPostId>.+)$', views.JobPostDetail.as_view()),
+    # need the '/' before $ to properly route call to generics.ListCreateAPIView
+    url(r'^jobPost$', views.JobPostList.as_view()),
 
-	url(r'^jobPost/allIds$',views.AllIdsList.as_view()),
+    # supports /jobPost/{jobPostId}
+    url(r'^jobPost/(?P<jobPostId>.+)$', views.JobPostDetail.as_view()),
+
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
--- JobPostService/job_post_app/views.py
+++ JobPostService/job_post_app/views.py
@@ -10,8 +10,8 @@ def index(request):
     return HttpResponse("Hello World! This is our Test App.")
 
 
-from rest_framework import generics
-from models import JobPost
+from rest_framework import generics, status
+from models import JobPost, Compensation
 from rest_framework.response import Response
 from rest_framework.views import APIView
 import json
@@ -39,6 +39,17 @@ class JobPostList(generics.ListCreateAPIView):
     queryset = JobPost.objects.all()
     serializer_class = JobPostSerializer
 
+    def create(self, request, *args, **kwargs):
+        job_post_data = request.data.pop('jobPost')
+        job_post_data['employerProfileId'] = job_post_data['employerProfile']['profileId']
+        job_post_data['locationId'] = job_post_data['location']['locationId']
+        # request.data has user info, not using it for now
+        job_post = JobPostSerializer(data=job_post_data)
+        if job_post.is_valid():
+            job_post.save()  # will call .create()
+            return Response(job_post.data, status=status.HTTP_201_CREATED)
+        else:
+            return Response(job_post.errors, status=status.HTTP_400_BAD_REQUEST)
 
 class JobPostDetail(generics.RetrieveUpdateDestroyAPIView):
     # override the default lookup field "PK" with the lookup field for this model
@@ -47,12 +58,19 @@ class JobPostDetail(generics.RetrieveUpdateDestroyAPIView):
     serializer_class = JobPostSerializer
 
 
-class JostPostBulkSave(APIView):
+class JobPostBulkSave(APIView):
     def post(self, request, format=None):
         data = request.data
         job_posts = data['jobPosts']
         JobPost.objects.bulk_create([JobPost(**json.loads(j)) for j in job_posts])
         return Response(len(job_posts))
+
+
+class CompensationList(APIView):
+    compensation = ['Hour', 'Day', 'Week', 'Month', 'Year'];
+
+    def get(self, request, format=None):
+        return Response(CompensationList.compensation)
 
 
 class AllIdsList(APIView):
